58dbeacd4c79e2faf79ba0b094bd04f5716806c4
Tim Jarzombek <tim@jarz.net>
2019-06-25 22:24:21 -0400

    lets not redirect nowhere

--- js/validation-functions.js
+++ js/validation-functions.js
@@ -10,19 +10,20 @@ $(document).ready(function() {
 
         // Use Ajax to submit form data
         var url = 'https://script.google.com/macros/s/AKfycby5YONrF_xGqMsfFZTn1ftiPNdqkFHLYCHdDga2n1hkVICLnkI/exec';
-        var redirectUrl = 'success-page.html';
         // show the loading 
         $('#postForm').prepend($('<span></span>').addClass('glyphicon glyphicon-refresh glyphicon-refresh-animate'));
         var jqxhr = $.post(url, $form.serialize(), function(data) {
             console.log("Success! Data: " + data.statusText);
-            $(location).attr('href',redirectUrl);
+            $('#rsvp-form').addClass('hidden');
+            $('#rsvp-thank-you').removeClass('hidden');            
         })
             .fail(function(data) {
                 console.warn("Error! Data: " + data.statusText);
                 // HACK - check if browser is Safari - and redirect even if fail b/c we know the form submits.
                 if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
                     //alert("Browser is Safari -- we get an error, but the form still submits -- continue.");
-                    $(location).attr('href',redirectUrl);                
+                    $('#rsvp-form').addClass('hidden');
+                    $('#rsvp-thank-you').removeClass('hidden');
                 }
             });
     });
